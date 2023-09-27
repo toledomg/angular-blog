98bd2ca67d1ade474f594a4c09aa8a59fd1d3643
toledomg <toledomg@yahoo.com.br>
2023-09-27 16:29:57 -0300

    :sparkles: feat: dinamizando small cards

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  smallImageCover: string = '';
 
+  @Input()
+  smallCardTitle: string = '';
+
+  @Input()
+  cardDescription: string = '';
+  constructor() {}
+  //
+  ngOnInit(): void {}
 }
